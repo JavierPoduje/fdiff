deea5f61f6036ea92117f67c7baa4dfa4e2eb9e0
JavierPoduje <javier.poduje@gmail.com>
2023-02-18 14:34:02 -0300

    feat: clean and refactor

--- src/git.rs
+++ src/git.rs
@@ -12,9 +12,34 @@ pub struct Commit {
 pub fn compare_branches(
     branch1: &str,
     branch2: &str,
-    exclude: Option<Vec<String>>,
-    repo_path: Option<PathBuf>,
+    words_to_exclude: Option<Vec<String>>,
+    maybe_repo_path: Option<PathBuf>,
 ) -> Result<Vec<Commit>, Box<dyn Error>> {
+    let repo_path = match get_repo_path(maybe_repo_path) {
+        Ok(repo_path) => repo_path,
+        Err(err) => return Err(err),
+    };
+
+    let raw_branch1_output = get_branch_commits(&repo_path, branch1).unwrap();
+    let raw_branch2_output = get_branch_commits(&repo_path, branch2).unwrap();
+
+    let raw_branch1_commits = parse_git_output(raw_branch1_output);
+    let raw_branch2_commits = parse_git_output(raw_branch2_output);
+    let branch1_commits: Vec<Commit> = raw_branch1_commits
+        .into_iter()
+        .filter_map(|msg| parse_commit_message(&msg, words_to_exclude.clone()))
+        .collect();
+    let branch2_commits: Vec<Commit> = raw_branch2_commits
+        .into_iter()
+        .filter_map(|msg| parse_commit_message(&msg, words_to_exclude.clone()))
+        .collect();
+
+    let commits = exclude(compare(branch1_commits, branch2_commits), words_to_exclude);
+
+    Ok(commits)
+}
+
+fn get_repo_path(repo_path: Option<PathBuf>) -> Result<String, Box<dyn Error>> {
     let mut git_log_cmd = Command::new("git");
 
     if let Some(repo_path) = repo_path {
@@ -29,52 +54,31 @@ pub fn compare_branches(
         return Err("Not inside a Git repository".into());
     }
 
-    let repo_path = String::from_utf8_lossy(&git_top_level_output.stdout)
+    Ok(String::from_utf8_lossy(&git_top_level_output.stdout)
         .trim()
-        .to_string();
-
-    let raw_branch1_output = get_branch_commits(&repo_path, branch1).unwrap();
-    let raw_branch2_output = get_branch_commits(&repo_path, branch2).unwrap();
-
-    let raw_branch1_commits = parse_git_output(raw_branch1_output);
-    let raw_branch2_commits = parse_git_output(raw_branch2_output);
-    let branch1_commits: Vec<Commit> = raw_branch1_commits
-        .into_iter()
-        .filter_map(|msg| parse_commit_message(&msg, exclude.clone()))
-        .collect();
-    let branch2_commits: Vec<Commit> = raw_branch2_commits
-        .into_iter()
-        .filter_map(|msg| parse_commit_message(&msg, exclude.clone()))
-        .collect();
-
-    let commits = compare(branch1_commits, branch2_commits, exclude);
-
-    Ok(commits)
+        .to_string())
 }
 
-fn compare(
-    commits1: Vec<Commit>,
-    commits2: Vec<Commit>,
-    exclude: Option<Vec<String>>,
-) -> Vec<Commit> {
+fn exclude(mut commits: Vec<Commit>, exclusions: Option<Vec<String>>) -> Vec<Commit> {
+    let to_exclude = if let Some(words) = exclusions {
+        HashSet::from_iter(words)
+    } else {
+        HashSet::new()
+    };
+    commits.retain(|commit| !to_exclude.contains(&commit.summary));
+    commits
+}
+
+fn compare(commits1: Vec<Commit>, commits2: Vec<Commit>) -> Vec<Commit> {
     let hash = commits2.iter().fold(HashSet::new(), |mut hash, commit| {
         hash.insert(commit.summary.to_string());
         hash
     });
 
-    let word_to_exclude = if let Some(words) = exclude {
-        words
-    } else {
-        Vec::new()
-    };
-
     let mut commits = Vec::new();
 
     for commit in commits1 {
-        let contains_excluded_word = word_to_exclude
-            .iter()
-            .any(|word| commit.summary.contains(word));
-        if !hash.contains(&commit.summary) && !contains_excluded_word {
+        if !hash.contains(&commit.summary) {
             commits.push(commit);
         }
     }
